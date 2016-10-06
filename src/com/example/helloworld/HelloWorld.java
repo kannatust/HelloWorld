59c027795b855cf003b9ef00bc74fba20d863ef3
Anna <kannatust@gmail.com>
2016-10-06 21:51:10 +0300

    ok

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,8 +1,15 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.example.helloworld;
 
 public class HelloWorld {
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
+    public HelloWorld() {
     }
 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
